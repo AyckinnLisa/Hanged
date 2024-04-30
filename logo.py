36c07377557b90bd9309befc15b8de976890bf29
Ayckinn Lisa <160609756+AyckinnLisa@users.noreply.github.com>
2024-04-30 15:44:19 +0200

    Create logo.py

---
+++ logo.py
@@ -0,0 +1,20 @@
+import colortx as ctx
+
+
+def logo():
+    return ctx.green('''
+ =======================================================================
+ =   HANGED - Célèbre jeu du pendu                                     =
+ =                                                                     =
+ =      - Version   : 1.0                                              =
+ =      - Autheur   : Ayckinn                                          =
+ =      - Site      : www.ayckinn.fr                                   =
+ =      - Mail      : ayckinn@icloud.com                               =
+ =      - Sortie    : May xx' 2024                                     =
+ =      - Github    : https://github.com/AyckinnLisa?tab=repositories  =
+ =      - Copyright : ©2024                                            =
+ =======================================================================
+
+ [Command/Windows + clic gauche] sur les liens de "Site" et "Github" pour afficher les pages\n
+ Entrer [!q] pour quitter le jeu.\n
+''')
